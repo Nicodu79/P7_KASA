01204752613c29dc46a194e13d9bfefee918fa68
nicolas you <cityhunter1234@hotmail.fr>
2022-10-24 16:41:41 +0200

    carrousel

---
+++ src/components/Carrousel.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const Carrousel = ({ images }) => {
+    let [imgActive, imgChange] = useState(0);
+    let imgNumber = images.length;
+
+    const imgPrevious = () => {
+        if (imgActive === 0) {
+            imgChange(imgNumber - 1);
+        } else {
+            imgChange(imgActive - 1);
+        }
+        return (imgChange);
+    };
+
+    const imgNext = () => {
+        if (imgActive === imgNumber - 1) {
+            imgChange(imgNumber = 0);
+        } else {
+            imgChange(imgActive + 1);
+        }
+        return (imgChange)
+    };
+
+    return (
+        <div className="carrousel">
+            {
+                imgNumber > 1 && <img className="arrow left-arrow" src="./assets/logo/arrow_left.png" alt="contenu précédent" onClick={imgPrevious} />
+            }
+            {
+                images.map((image, index) => {
+                    return (
+                        <img key={index} className={index === imgActive ? "carrousel-img actif" : "carrousel-img"} src={image} alt="Logement" />
+                    )
+                })
+            }
+            {
+                imgNumber > 1 && <img className="arrow right-arrow" src="./assets/logo/arrow_right.png" alt="contenu suivant" onClick={imgNext} />
+            }
+        </div>
+    );
+};
+
+export default Carrousel;
--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Logements from "../components/Logements";
+import Carrousel from "../components/Carrousel";
 
 const Logement = () => {
     return (
